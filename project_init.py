3bec87cdc255b559b3d91b32be419850ca3d11aa
akshat-shah-2003 <akshats1607@gmail.com>
2025-10-11 11:14:54 +0530

    project directory setup complete

---
+++ project_init.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+import os
+
+files_list = [
+    "data/raw/",
+    "data/processed/",
+    "logs/",
+    "etl/extract.py",
+    "etl/transform.py",
+    "etl/load.py",
+    "etl/run_etl.py",
+    "utils/logger.py",
+    "utils/metrics.py"
+]
+
+for eachfile in files_list:
+    filepath = Path(eachfile)
+    filedir, filename = os.path.split(filepath)
+    if filedir != "":
+        os.makedirs(filedir, exist_ok = True)
+    if not filepath.exists():
+        with open(filepath, "w") as f:
+            pass
+        print(f"Created file: {eachfile}")
+    else:
+        print(f"File already exists: {eachfile}")
