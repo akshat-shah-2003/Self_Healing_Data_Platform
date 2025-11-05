267cd79d202b55f1cc869518ff586fa522c3fc55
akshat-shah-2003 <akshats1607@gmail.com>
2025-11-05 22:33:56 +0530

    snapshot load and save logic

--- etl/extraction.py
+++ etl/extraction.py
@@ -10,9 +10,49 @@ logger = get_logger(__name__)
 
 # Load environment variables from .env file
 load_dotenv()
+output_dir = "data/db_metadata"
+os.makedirs(output_dir, exist_ok=True)
+
+def load_snapshot():
+    """Load the most recent snapshot file (if any)."""
+    files = sorted(
+        [f for f in os.listdir(output_dir) if f.endswith("_snapshot.json")]
+    )
+    if not files:
+        return None
+    latest_file = os.path.join(output_dir, files[-1])
+    with open(latest_file, "r") as f:
+        return json.load(f)
+
+def save_snapshot(snap):
+    """Save the snapshot to a JSON file."""
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    file_path = os.path.join(output_dir, f"{timestamp}_snapshot.json")
+    with open(file_path, "w") as f:
+        json.dump(snap, f, indent=4)
+    logger.info(f"Snapshot saved to {file_path}")
+
+def to_map(snap):
+    if not snap:
+        return {}
+    flat_map = {}
+    for schema_name, schema_data in snap.get("schemas", {}).items():
+        for table_name, columns in schema_data.get("tables", {}).items():
+            for col in columns:
+                kkey = f"{schema_name}.{table_name}.{col['column_name']}"
+                flat_map[kkey] = col
+    return flat_map
+
+def compare_snapshots(old_snap, new_snap):
+    old_map = to_map(old_snap)
+    new_map = to_map(new_snap)
+    pass
 
 
 def infer_schema():
+    ss = load_snapshot()
+    logger.info(f"Loaded latest snapshot.")
+    
     """Connect to Snowflake, infer schema metadata, and save it as a JSON file."""
     try:
         # Establish Snowflake connection
@@ -42,10 +82,7 @@ def infer_schema():
             FROM {os.getenv("SNOWFLAKE_DATABASE")}.INFORMATION_SCHEMA.COLUMNS
             WHERE table_schema = '{os.getenv("SNOWFLAKE_SCHEMA")}';
         """
-        database = os.getenv("SNOWFLAKE_DATABASE")
         schema = os.getenv("SNOWFLAKE_SCHEMA")
-        user_id = os.getenv("SNOWFLAKE_USER")
-        timestamp = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
 
         metadata = {
             "user_id": os.getenv("SNOWFLAKE_USER"),
@@ -69,21 +106,20 @@ def infer_schema():
                 "is_nullable": is_nullable,
                 "default": column_default,
             })
-        
-        #print(metadata)
-
-        # Ensure directory exists
-        output_dir = "data/db_metadata"
-        os.makedirs(output_dir, exist_ok=True)
 
         # Define file path
-        file_path = f"{output_dir}/{os.getenv('SNOWFLAKE_DATABASE')}_{datetime.now().strftime('%Y%m%d_%H%M%S')}_metadata_snapshot.json"
+        file_path = f"{output_dir}/{datetime.now().strftime('%Y%m%d_%H%M%S')}_snapshot.json"
 
         # Save schema to JSON
         with open(file_path, "w") as f:
             json.dump(metadata, f, indent=4)
 
         logger.info(f"Schema inference completed and saved to: {file_path}")
+
+        snapshot = json.load(open(os.path.join(output_dir,ss), "r"))
+
+        print(type(snapshot))
+        #ss = file_path
 
     except Exception as e:
         logger.error(f"Schema inference failed: {e}")
