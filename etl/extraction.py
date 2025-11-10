cf95ebb87caea4b4f20fde7018e798ddd865697e
akshat-shah-2003 <akshats1607@gmail.com>
2025-11-11 00:22:53 +0530

    schema healing

--- etl/extraction.py
+++ etl/extraction.py
@@ -3,7 +3,7 @@ import json
 import snowflake.connector
 from dotenv import load_dotenv
 from utils.logger import get_logger
-from datetime import datetime
+from datetime import datetime, timezone
 from difflib import SequenceMatcher
 
 # Initialize logger
@@ -45,10 +45,44 @@ def to_map(snap):
             for col in columns:
                 kkey = f"{schema_name}.{table_name}.{col['column_name']}"
                 flat_map[kkey] = col
-                # {PUBLIC.SALES_DATA.CONTACTLASTNAME: {"column_name": "CONTACTLASTNAME","data_type": "TEXT","is_nullable": "YES","default": null},...}
+    # {PUBLIC.SALES_DATA.CONTACTLASTNAME: {"column_name": "CONTACTLASTNAME","data_type": "TEXT","is_nullable": "YES","default": null},...}
     return flat_map
 
-def compare_snapshots(old_snap, new_snap):
+def to_snap(flat_map, user_id="AKSHATSHAH", database="SALES_DB", timestamp = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ")):
+
+    if not flat_map:
+        return {
+            "user_id": user_id,
+            "timestamp": timestamp,
+            "database": database,
+            "schemas": {}
+        }
+
+    snap = {
+        "user_id": user_id,
+        "timestamp": timestamp,
+        "database": database,
+        "schemas": {}
+    }
+
+    for key, col_data in flat_map.items():
+        parts = key.split(".")
+        if len(parts) != 3:
+            raise ValueError(f"Invalid key format: {key}")
+        schema_name, table_name, column_name = parts
+
+        schemas = snap.setdefault("schemas", {})
+        schema_obj = schemas.setdefault(schema_name, {})
+        tables = schema_obj.setdefault("tables", {})
+        table_cols = tables.setdefault(table_name, [])
+
+        table_cols.append(col_data)
+
+
+    return snap
+
+
+def compare_schema(old_snap, new_snap):
     if not old_snap:
         logger.info("No previous snapshot found. Skipping comparison.")
         return
@@ -71,14 +105,38 @@ def compare_snapshots(old_snap, new_snap):
     # print("************************************")
     # print(old_snap)
     # print("************************************")
-
+    renamed = {}
     for rem in removed:
         for add in added:
             if is_probable_rename(old_snap_d[rem]["column_name"], new_snap_d[add]["column_name"]) and old_snap_d[rem]["data_type"] == new_snap_d[add]["data_type"]:
                 logger.warning(f"Column rename detected: {rem} => {add}")
+                renamed[rem] = add
+    
+    return {"added": added, "removed": removed, "common": common, "renamed": renamed}
 
+def heal_schema(diff_status, old_snap, new_snap):
+    if not diff_status or not old_snap:
+        return
+    old_snap_d = to_map(old_snap)
+    new_snap_d = to_map(new_snap)
+    logger.info(f"Newly added columns:, {diff_status['added']}")
+    for i in diff_status["added"]:
+        if i not in diff_status["renamed"].values():
+            uinp1 = input(f"Do you want to remove added column '{i}'?(Y/N): ").strip().upper()
+            if uinp1 == "Y":
+                uinp2 = input("Changes will be permanent! Are you sure?(Y/N): ").strip().upper()
+                if uinp2 == "Y":
+                    new_snap_d.pop(i)
+            logger.info("Column not removed.")
 
+    logger.info(f"Removed columns: {diff_status['removed']}")
+    for j in diff_status["removed"]:
+        if j not in diff_status["renamed"].keys():
+            uinp3 = input(f"Do you want to restore deleted column '{j}'?(Y/N): ").strip().upper()
+            if uinp3 == "Y":
+                new_snap_d[j] = old_snap_d[j]
 
+    return to_snap(new_snap_d)
 
 
 def infer_schema():
@@ -109,11 +167,11 @@ def infer_schema():
                 table_name,
                 column_name,
                 data_type, 
-                ordinal_position, 
                 is_nullable, 
                 column_default 
             FROM {os.getenv("SNOWFLAKE_DATABASE")}.INFORMATION_SCHEMA.COLUMNS
             WHERE table_schema = '{os.getenv("SNOWFLAKE_SCHEMA")}'
+            AND table_name = 'SALES_DATA'
             ORDER BY ordinal_position;
         """
         schema = os.getenv("SNOWFLAKE_SCHEMA")
@@ -132,18 +190,25 @@ def infer_schema():
 
         schema_dict = metadata["schemas"][schema]["tables"]
 
-        for table_name, column_name, data_type, ordinal_position, is_nullable, column_default in cur.fetchall():
+        for table_name, column_name, data_type, is_nullable, column_default in cur.fetchall():
             schema_dict.setdefault(table_name,[]).append({
                 "column_name": column_name,
                 "data_type": data_type,
-                "ordinal_position": ordinal_position,
                 "is_nullable": is_nullable,
                 "default": column_default
             })
-        
-        #print(metadata)
-        compare_snapshots(ss, metadata)
+        logger.info("Saving snapshot before healing...")
         save_snapshot(metadata)
+        #print(metadata)
+        diff_stat = compare_schema(ss, metadata)
+        #print(diff_stat)
+        fns = heal_schema(diff_stat, ss, metadata)
+        if not fns:
+            logger.info("No healing performed. Snapshot already saved.")
+        else:
+            logger.info("Saving snapshot after healing...")
+            save_snapshot(fns)
+        
         #ss = file_path
 
     except Exception as e:
